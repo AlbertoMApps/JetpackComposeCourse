71e2c9d435fef90d44883669649e59077f04ba5f
Alberto Medrano <albersna@hotmail.com>
2023-08-15 16:40:49 +0100

    Added compose for material design course 2

--- app/src/main/java/com/alberto/compose/ui/MainActivity.kt
+++ app/src/main/java/com/alberto/compose/ui/MainActivity.kt
@@ -1,17 +1,23 @@
-package com.alberto.compose
+package com.alberto.compose.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.alberto.compose.ui.messages
 import com.alberto.compose.ui.screens.compose_essentials.Conversation
 import com.alberto.compose.ui.screens.compose_essentials.MyApp
 import com.alberto.compose.ui.screens.compose_essentials.MySootheApp
 import com.alberto.compose.ui.screens.compose_essentials.WellnessScreen
+import com.alberto.compose.ui.screens.material_design.ReplyApp
+import com.alberto.compose.ui.screens.material_design.ReplyHomeViewModel
 import com.alberto.compose.ui.theme.JetpackComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +41,22 @@ class MainActivity : ComponentActivity() {
                     WellnessScreen()
 
                     //Material design, layouts, theming and animation
+                    val viewModel: ReplyHomeViewModel by viewModels()
+                    val uiState by viewModel.uiState.collectAsState()
 
-
+                    Surface(
+                        tonalElevation = 5.dp
+                    ) {
+                        ReplyApp(
+                            replyHomeUIState = uiState,
+                            closeDetailScreen = {
+                                viewModel.closeDetailScreen()
+                            },
+                            navigateToDetail = { emailId ->
+                                viewModel.setSelectedEmail(emailId)
+                            }
+                        )
+                    }
                 }
             }
         }
